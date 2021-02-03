11a149728068423b5633b0a88ffbcecec470e1d1
YI <yurkimus@gmail.com>
2021-02-03 23:50:56 +0300

    purescript: setup chapter 3

---
+++ purescript/purecript-by-example/src/Chapters/Three.purs
@@ -0,0 +1,69 @@
+module Chapter.Three where
+
+import Prelude
+
+import Control.Plus (empty)
+import Data.List (List(..), Cons, filter, head, insert, nubBy)
+import Data.Maybe (Maybe)
+
+-- functions
+add :: Int -> Int -> Int
+add x y = x + y
+
+sub :: Int -> Int -> Int
+sub = \x y -> x - y
+
+mult :: Int -> Int -> Int -> Int
+mult x y z = first * second
+  where -- local scope
+    first = add x y
+    second = add y z
+
+-- example
+type Address = { street :: String, city :: String, state :: String }
+
+type Entry = { firstName :: String, lastName ::String, address :: Address }
+
+type AddressBook = List Entry
+
+emptyBook :: AddressBook
+emptyBook = empty
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry entry = insert entry
+
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName = head <<< filter filterEntry
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter filterEntry
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.address.street == street
+
+findEntryByName :: String -> AddressBook -> Maybe Entry
+findEntryByName firstName = head <<< filter filterEntry
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubBy filterEntry
+  where
+    filterEntry :: Entry -> Entry -> Boolean
+    filterEntry prev next = prev.firstName == next.firstName
+
+showEntry :: Entry -> String
+showEntry entry =
+  entry.lastName <> ", " <>
+  entry.firstName <> ", " <>
+  showAddress entry.address
+
+showAddress :: Address -> String
+showAddress address =
+  address.street <> ", " <>
+  address.city <> ", " <>
+  address.state
